50b78e293dd6be51927fd49ee3e37c6b56617f66
Gweedzy <pj.leborgne@gmail.com>
2016-04-10 18:02:12 +0200

    Contact Form designed

---
+++ contact.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Contact form</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="contact.css">
+        <link href='https://fonts.googleapis.com/css?family=Poppins:300,500' rel='stylesheet' type='text/css'>
+    </head>
+    
+    <body>
+        <div id="contact">
+            <h2>Contactez-nous !</h2>
+            <p class="about">Les échanges se feront par courrier électronique, merci de saisir une adresse e-mail valide vous appartenant. Nous serons les seuls à avoir accès à vos informations.</p>
+            <form>
+                <input type="text" name="nom" placeholder="Nom">
+                <input type="text" name="nom_entreprise" placeholder="Entreprise">
+                <input type="email" name="email" placeholder="Adresse e-mail">
+                <textarea name="content" placeholder="Contenu du message..." rows="5"></textarea>
+                <input type="submit" name="send" value="Envoyer le message">
+            </form>
+        </div>
+    </body>
+</html>
